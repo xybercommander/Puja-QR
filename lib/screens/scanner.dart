0ced70617ba4a82eb30a65ed8fbe2f1d4a278088
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-06-25 21:20:12 +0530

    finished implementing the scanner page ui

--- lib/screens/scanner.dart
+++ lib/screens/scanner.dart
@@ -11,9 +11,51 @@ class _ScannerState extends State<Scanner> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Scanner Page'),
-      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(horizontal: 36),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),              
+              child: Text(
+                'Scan a \nQR Code',
+                style: TextStyle(
+                  fontFamily: 'Nunito-Black',
+                  color: Theme.of(context).accentColor,
+                  fontSize: 32
+                ),
+              ),
+            ),
+            Center(
+              child: Image.asset('assets/images/qr_scan_image.png'),
+            ),
+            Container(                    
+              height: 55,
+              child: RawMaterialButton(     
+                elevation: 0,               
+                fillColor: Theme.of(context).primaryColor,
+                shape: StadiumBorder(),
+                onPressed: () {
+
+                },
+                child: Center(
+                  child: Text(
+                    'Scan',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,                          
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
